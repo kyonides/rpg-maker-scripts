e414e6dd527009bf31ae1a51420e5ac5a94b676c
kyonides <kyonides@gmail.com>
2026-06-17 21:42:40 -0600

    Added KTouchNewMapEvent XP + VX + ACE

---
+++ rmvxace/ktouchnewmapevent_ace.rb
@@ -0,0 +1,38 @@
+# * KTouchNewMapEvent ACE
+#   Scripter : Kyonides Arkanthes
+#   2022-09-29 - Edited 2026-06-17
+
+# * Plug & Play Script * #
+
+# This scriptlet allows you to run an event after being transferred to another
+# map. It will run even if the trigger were the Player Touch or Event Touch one.
+
+# * Aliased Method: Game_Event#setup_page
+
+class Game_Player
+  attr_reader :new_x, :new_y
+end
+
+class Game_Event
+  alias :kyon_touch_new_map_event_setup_page :setup_page
+  def touch_trigger?
+    [1, 2].include?(@trigger)
+  end
+
+  def player_new_x?
+    $game_player.new_x == @x
+  end
+
+  def player_new_y?
+    $game_player.new_y == @y
+  end
+
+  def check_shared_location
+    start if touch_trigger? and player_new_x? and player_new_y?
+  end
+
+  def setup_page(new_page)
+    kyon_touch_new_map_event_setup_page(new_page)
+    check_shared_location
+  end
+end
